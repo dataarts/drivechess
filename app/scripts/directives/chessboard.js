b036345b5904b11b8cfb5ea167081d2fe153e6db
Doug Fritz <dougfritz@google.com>
2013-03-22 17:29:38 -0700

    chess template

--- app/scripts/directives/chessboard.js
+++ app/scripts/directives/chessboard.js
@@ -1,12 +1,21 @@
 'use strict';
 
 angular.module('chessApp')
-  .directive('chessboard', function () {
+  .directive('chessboard', function() {
+    var i, j, template;
+    var letters = 'abcdefgh';
+    for (i = 0; i < 8; i++) {
+      var letter = letters[i];
+      for (j = 0; j < 8; j++) {
+        template += '<div id="' + (letter + (j + 1)) + '"></div>\n';
+      }
+    }
     return {
-      templateUrl: 'scripts/directives/chessboard.html',
+      template: template,
       restrict: 'E',
+      replace: false,
+      transclude: true,
       link: function postLink(scope, element, attrs) {
-        element.text('this is the chessboard directive');
       }
     };
   });
