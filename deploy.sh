8462d57615528d5f8273b65aea7699509f20fbed
Doug Fritz <dougfritz@google.com>
2013-04-19 17:59:26 -0700

    updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,8 @@ cd client
 grunt build --force
 git checkout gh-pages
 cd ..
-mv -f client/dist/* .
+foreach i (*) if [ $i != "client" ]; then rm -rf $i; fi; end;
+mv -f client/dist/* ./
 git add .
 git commit -am 'update dist'
 git push
