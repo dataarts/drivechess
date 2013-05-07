d6eb83c5c507b0941b2f8d9129a8c4fd40181244
Aleksandar Rodic <aleksandar.xyz@gmail.com>
2013-05-07 15:29:04 -0700

    updated build script

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,6 @@ foreach i (*) if [ $i != "client" ]; then rm -rf $i; fi; end;
 mv -f client/dist/* ./
 git add .
 git commit -am 'update dist'
-git push
+git push origin gh-pages
 git checkout master
 rm -rf components
